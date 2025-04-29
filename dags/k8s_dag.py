70801ccd61e97eca0e5ea9b9c446c01de8c74acc
goeun <akfmdl@naverc.om>
2025-04-29 13:13:20 +0900

    Update k8s DAG resource configuration to include GPU requests and limits for improved compatibility with NVIDIA resources.

--- dags/k8s_dag.py
+++ dags/k8s_dag.py
@@ -188,8 +188,8 @@ with DAG(
         volume_mounts=[work_dir_volume_mount, dags_dir_volume_mount, shm_volume_mount],
         env_vars=env_vars,
         container_resources=k8s.V1ResourceRequirements(
-            requests={"cpu": "1", "memory": "4Gi"},  # GPU가 있을 경우 nvidia.com/gpu 항목 추가
-            limits={"cpu": "2", "memory": "8Gi"},  # GPU가 있을 경우 nvidia.com/gpu 항목 추가
+            requests={"cpu": "1", "memory": "4Gi", "nvidia.com/gpu": 1},  # GPU가 없을 경우 nvidia.com/gpu 항목 제거
+            limits={"cpu": "2", "memory": "8Gi", "nvidia.com/gpu": 1},  # GPU가 없을 경우 nvidia.com/gpu 항목 제거
         ),
         is_delete_operator_pod=True,
         in_cluster=True,
