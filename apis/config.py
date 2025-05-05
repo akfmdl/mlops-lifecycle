7ea4aa8bede678bd7169c638d1d23bb8724a7365
goeun <akfmdl@naverc.om>
2025-05-05 16:59:50 +0900

    Update configuration and Dockerfile for improved deployment; remove unused Helm templates. Set TRITON_URL to an empty string, install necessary libraries in Dockerfile, and update Airflow image reference. Remove deprecated deployment, HPA, ingress, and service templates from Helm chart.

--- apis/config.py
+++ apis/config.py
@@ -7,7 +7,7 @@ class Config(BaseSettings):
     MODEL_NAMESPACE: str = os.getenv("MODEL_NAMESPACE", "mlops-platform")
     UVICORN_HOST: str = os.getenv("UVICORN_HOST", "localhost")
     UVICORN_PORT: int = int(os.getenv("UVICORN_PORT", "8888"))
-    TRITON_URL: str = os.getenv("TRITON_URL", "localhost:30401")
+    TRITON_URL: str = os.getenv("TRITON_URL", "")
 
 
 settings = Config(_env_file=None)
