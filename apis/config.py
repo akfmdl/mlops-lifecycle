84556f5c775bb8e1675677deaa236e5678672260
gorani <akfmdl@naver.com>
2025-05-21 07:43:01 +0000

    Remove MODEL_NAMESPACE configuration from config.py and values.yaml to streamline environment variable usage.

--- apis/config.py
+++ apis/config.py
@@ -5,7 +5,6 @@ from pydantic_settings import BaseSettings
 
 class Config(BaseSettings):
     APP_NAME: str = os.getenv("APP_NAME", "fastapi-app")
-    MODEL_NAMESPACE: str = os.getenv("MODEL_NAMESPACE", "mlops-platform")
     SERVICE_HOST: str = os.getenv("SERVICE_HOST", "localhost")
     SERVICE_PORT: int = int(os.getenv("SERVICE_PORT", "8888"))
     ONNX_MODEL_TRITON_URL: str = os.getenv("ONNX_MODEL_TRITON_URL", "localhost:8000")
